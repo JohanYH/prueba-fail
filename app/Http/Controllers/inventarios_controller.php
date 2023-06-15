967133918f368b2b5540c1c3fcab66eff66e4bac
Juan Diego <juandiegoramirez098@gmail.com>
2023-06-15 18:01:55 -0500

    feat: :sparkles:

--- app/Http/Controllers/inventarios_controller.php
+++ app/Http/Controllers/inventarios_controller.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Inventario;
 
 class inventarios_controller extends Controller
 {
-    public function insert_inventario(){
+    public function insert_inventario(Request $req){
 
-        return "xd";
+
+        $data = Inventario::where("id_bodega", "=", $req -> id_bodega)->where("id_producto", "=", $req -> id_producto)->get();
+
+        return $data;
     }
 }
